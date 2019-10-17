5155405205ab597805859171c09771f2a6f4ac50
Christian T <weareme99@gmail.com>
2019-10-17 20:28:28 +0000

    added testing and functioning show elements for gram

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,6 +5,13 @@ class GramsController < ApplicationController
   def new
   end
 
+  def show
+    @gram = @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
+  end
+
   def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
