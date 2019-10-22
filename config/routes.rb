9f832624b6af6f085685af5b33ab80fa412b4b5b
Christian T <weareme99@gmail.com>
2019-10-22 00:41:39 +0000

    added destroy functionality and tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "grams#index"
   resources :grams
-  resources :grams, only: [:new, :create, :show, :edit, :update]
 end
