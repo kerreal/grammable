a7ad407ba67bf00795689897c18f0e3569db96b5
Christian T <weareme99@gmail.com>
2019-10-23 01:48:06 +0000

    added picture uploading with aws support

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,9 +1,11 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
+    @grams = Gram.all
   end
 
   def new
+    @gram = Gram.new
   end
 
   def show
@@ -50,7 +52,7 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
   def render_not_found(status=:not_found)
